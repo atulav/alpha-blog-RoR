65aa07432080fa96fdb2c91957885795eeabef48
atulav <atvicsh@gmail.com>
2021-06-06 18:55:32 +0000

    Deleting, editing and Linking

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,13 +3,21 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
     
+    
     def index
         @articleAll = Article.all
     end
     
+    
     def new
         @article = Article.new
     end
+    
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
     
     def create
        @article = Article.new(params.require(:article).permit(:title, :description)) 
@@ -20,5 +28,24 @@ class ArticlesController < ApplicationController
        else
           render 'new'
        end
+    end
+    
+    
+    def update
+        @article = Article.find(params[:id])
+        
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was edited successfully!"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+    
+    
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
     end
 end
